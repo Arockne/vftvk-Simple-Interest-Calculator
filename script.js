5e51bd23cc707f4bb062d0a6b9f924db5ed7b965
Daniel Calvin <arockne2003@yahoo.com>
2022-11-11 19:33:21 -0700

    updateRate will pass the node that it is called upon

--- script.js
+++ script.js
@@ -13,7 +13,6 @@ function compute() {
     result.innerHTML = `If you deposit <mark>${principal}</mark>,<br/>at an interest rate of <mark>${rate}</mark>.<br/>You will receive an amount of <mark>${interest}</mark>,<br/>in the year <mark>${year}</mark><br/><br/>`
 }
 
-function updateRate() {
-    var rateval = document.getElementById("rate").value;
-    document.getElementById("rate_val").innerText = `${rateval}%`;
+function updateRate(rate) {
+    document.getElementById("rate_val").innerText = `${rate.value}%`;
 }
