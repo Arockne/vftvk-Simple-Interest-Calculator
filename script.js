c3084e5ca8dc264282f4cdd7271bab32c3076975
Daniel Calvin <arockne2003@yahoo.com>
2022-11-11 21:03:01 -0700

    refactor to seperate logic

--- script.js
+++ script.js
@@ -1,18 +1,29 @@
 function compute() {
     const principal = document.getElementById("principal").value;
+
     if (principal <= 0) {
         alert('Enter a positive number')
         document.getElementById("principal").focus()
         return
     }
+
     const rate = document.getElementById("rate").value;
     const years = document.getElementById("years").value;
-    const interest = principal * years * rate / 100;
-    const year = new Date().getFullYear() + parseInt(years)
+    const interest = calculateInterest(principal, years, rate)
+    const year = calculateYear(years)
+    
     const result = document.getElementById("result")
-    result.innerHTML = `If you deposit <mark>${principal}</mark>,<br/>at an interest rate of <mark>${rate}</mark>.<br/>You will receive an amount of <mark>${interest}</mark>,<br/>in the year <mark>${year}</mark><br/><br/>`
+    result.innerHTML = calculatedResults(principal, rate, interest, year)
 }
 
 function updateRate(rate) {
     document.getElementById("rate_val").innerText = `${rate.value}%`;
 }
+
+function calculatedResults(principal, rate, interest, year) {
+    return `If you deposit <mark>${principal}</mark>,<br/>at an interest rate of <mark>${rate}</mark>.<br/>You will receive an amount of <mark>${interest}</mark>,<br/>in the year <mark>${year}</mark><br/><br/>`
+}
+
+const calculateInterest = (principal, years, rate) => principal * years * rate / 100;
+
+const calculateYear = (years) => new Date().getFullYear() + parseInt(years);
